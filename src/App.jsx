e3a8b4b69487db0ca68e56878460457fe8736eb9
SHADOW-1104 <ing.colin@outlook.com>
2024-07-22 15:28:18 -0600

    mejora de boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from "react"
 
 export const App = () => {
+  const val = 201;
 
   const [cont, setCont] = useState(null)
-
+  
   const handleSuma = () => {
     setCont(cont + 1)
   }
-
+  
   return (
     <>
-      <h1>Contador</h1>
+    <div className="container">
+      <h2>Contador :</h2>
       {cont && <h2>{cont}</h2>}
+    </div>
       <button className="btn" type='button' onClick={handleSuma}>Click me!</button>
+      
     </>
   )
 }
